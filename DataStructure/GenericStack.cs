bb10e355d126b7e56b7079ea4ece28278cee69fe
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-19 15:42:10 +0530

    [sanjana] Add : Generic stack

---
+++ DataStructure/GenericStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class GenericStack<T>
+    {
+        private Node<T> top;
+        public GenericStack()
+        {
+            this.top = null;
+        }
+        internal void Push(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node<T> temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is Empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the TOP of the stack", this.top);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is Empty");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+    }
+}
+
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,11 +11,17 @@ namespace DataStructure
 
         public static void Main(string[] args)
         {
-            GenericLinkedList<int> list = new GenericLinkedList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Display();
+            //GenericLinkedList<int> list = new GenericLinkedList<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Display();
+            GenericStack<int> stack = new GenericStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Display();
+            stack.Pop();
         }
 
 
